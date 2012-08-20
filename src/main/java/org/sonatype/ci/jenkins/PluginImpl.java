195a45dbecc952602ae2ddf776376824d53a28b1
Stuart McCulloch <mcculls@gmail.com>
2012-08-20 19:00:40 +0100

    Batch up site updates; no need to switch auth since plugin install should run as system

--- src/main/java/org/sonatype/ci/jenkins/PluginImpl.java
+++ src/main/java/org/sonatype/ci/jenkins/PluginImpl.java
@@ -6,38 +6,38 @@
  */
 package org.sonatype.ci.jenkins;
 
+import hudson.BulkChange;
 import hudson.Plugin;
 import hudson.init.InitMilestone;
 import hudson.init.Initializer;
 import hudson.model.Hudson;
 import hudson.model.UpdateCenter;
 import hudson.model.UpdateSite;
-import hudson.security.ACL;
+import hudson.util.PersistedList;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.acegisecurity.Authentication;
-import org.acegisecurity.context.SecurityContextHolder;
-
-public class PluginImpl
+public final class PluginImpl
     extends Plugin
 {
     private static final Logger log = Logger.getLogger( PluginImpl.class.getName() );
 
     @Initializer( after = InitMilestone.EXTENSIONS_AUGMENTED, attains = "sonatype-update-sites" )
     public static void addUpdateSites()
-        throws Exception
+        throws IOException
     {
         log.info( "Adding Sonatype update site(s)" );
 
-        UpdateCenter updateCenter = Hudson.getInstance().getUpdateCenter();
-        if ( updateCenter.getSites().isEmpty() )
-        {
-            updateCenter.load();
-        }
+        final UpdateCenter updateCenter = Hudson.getInstance().getUpdateCenter();
 
-        addUpdateSite( updateCenter, "insight-ci", "http://links.sonatype.com/products/insight/ci/update-site" );
+        final UpdateSite[] updateSites =
+            { new UpdateSite( "insight-ci", "http://links.sonatype.com/products/insight/ci/update-site" ) };
+
+        addUpdateSites( updateCenter, updateSites );
     }
 
     @Initializer( requires = "sonatype-update-sites" )
@@ -45,58 +45,78 @@ public class PluginImpl
     {
         log.info( "Installing Sonatype plugin(s)" );
 
-        UpdateCenter updateCenter = Hudson.getInstance().getUpdateCenter();
+        final UpdateCenter updateCenter = Hudson.getInstance().getUpdateCenter();
 
         installPlugin( updateCenter, "insight-ci" );
     }
 
-    private static void addUpdateSite( UpdateCenter updateCenter, String name, String url )
+    private static void addUpdateSites( final UpdateCenter updateCenter, final UpdateSite... updateSites )
+        throws IOException
     {
-        if ( updateCenter.getById( name ) == null )
+        final List<UpdateSite> newSites = new ArrayList<UpdateSite>();
+        final List<UpdateSite> oldSites = new ArrayList<UpdateSite>();
+
+        for ( final UpdateSite newSite : updateSites )
         {
-            @SuppressWarnings( "static-access" )
-            Authentication authentication = Hudson.getAuthentication();
+            final String id = newSite.getId();
+            final UpdateSite oldSite = updateCenter.getById( id );
+            String action = null;
+
+            if ( oldSite == null )
+            {
+                action = "Adding ";
+                newSites.add( newSite );
+            }
+            else if ( !newSite.getUrl().equals( oldSite.getUrl() ) )
+            {
+                action = "Updating ";
+                oldSites.add( oldSite );
+                newSites.add( newSite );
+            }
+
+            if ( action != null )
+            {
+                log.info( action + id + " [" + newSite.getUrl() + "]" );
+            }
+        }
+
+        if ( !newSites.isEmpty() )
+        {
+            final BulkChange bc = new BulkChange( updateCenter );
             try
             {
-                SecurityContextHolder.getContext().setAuthentication( ACL.SYSTEM );
-
-                log.info( "Adding " + name + "..." );
-                updateCenter.getSites().add( new UpdateSite( name, url ) );
-                log.info( "Added " + name );
-            }
-            catch ( Exception e )
-            {
-                log.log( Level.WARNING, "Cannot add " + name, e );
+                final PersistedList<UpdateSite> sites = updateCenter.getSites();
+                if ( sites.isEmpty() )
+                {
+                    updateCenter.load();
+                }
+                for ( final UpdateSite oldSite : oldSites )
+                {
+                    sites.remove( oldSite );
+                }
+                sites.addAll( newSites );
             }
             finally
             {
-                SecurityContextHolder.getContext().setAuthentication( authentication );
+                bc.commit();
             }
         }
     }
 
-    private static void installPlugin( UpdateCenter updateCenter, String name )
+    private static void installPlugin( final UpdateCenter updateCenter, final String name )
     {
-        UpdateSite.Plugin plugin = updateCenter.getPlugin( name );
+        final UpdateSite.Plugin plugin = updateCenter.getPlugin( name );
         if ( plugin != null && plugin.getInstalled() == null )
         {
-            @SuppressWarnings( "static-access" )
-            Authentication authentication = Hudson.getAuthentication();
             try
             {
-                SecurityContextHolder.getContext().setAuthentication( ACL.SYSTEM );
-
                 log.info( "Installing " + name + "..." );
                 plugin.deploy().get();
                 log.info( "Installed " + name );
             }
-            catch ( Exception e )
+            catch ( final Exception e )
             {
                 log.log( Level.WARNING, "Cannot install " + name, e );
-            }
-            finally
-            {
-                SecurityContextHolder.getContext().setAuthentication( authentication );
             }
         }
     }
